c85549f2f26c48d93507f40c84b389f28d296b44
Francesco Pretto <ceztko@gmail.com>
2017-06-11 20:05:11 +0200

    Different GUID for VS10 version

--- SccAutoSwitcher/Guids.cs
+++ SccAutoSwitcher/Guids.cs
@@ -5,6 +5,10 @@ namespace SccAutoSwitcher
 {
     static class GuidList
     {
+#if VS10
+        public const string guidPkgString = "e0602ccb-aef8-4c8a-a598-530ff1be851b";
+#else
         public const string guidPkgString = "99316bc5-70c5-4ef1-9a29-ea7568408ed0";
+#endif
     };
 }
